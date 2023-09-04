005962c0ad4c82e0ec71fd5cdbc596dc8b21be38
JoshAllenB <batayolajoshuaallen@gmail.com>
2023-09-04 19:05:21 +0800

    Fixed some JS logic added remove and read status add styling css

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ closeBtn.addEventListener("click", () => {
 });
 
 // Adding Book Function
-
 // Book array
 const books = [];
 
@@ -61,31 +60,60 @@ function displayBooks() {
       readText = 'No';
     }
 
-    // Create card element
-    const card = document.createElement('div');
-    card.classList.add('card');
+// Create card element
+const card = document.createElement('div');
+  card.classList.add('card');
+// Create card content
+const title = document.createElement('h3');
+  title.textContent = book.title;
 
-    // Create card content
-    const title = document.createElement('h3');
-    title.textContent = book.title;
+const author = document.createElement('p');
+  author.textContent = `Author: ${book.author}`;
 
-    const author = document.createElement('p');
-    author.textContent = `Author: ${book.author}`;
+const pages = document.createElement('p'); 
+  pages.textContent = `Pages: ${book.pages}`;
 
-    const pages = document.createElement('p'); 
-    pages.textContent = `Pages: ${book.pages}`;
+const readStatusSelectDiv = document.createElement('div');
+  readStatusSelectDiv.classList.add('statusContainer');
 
-    const read = document.createElement('p');
-    read.textContent = `Have you read it? ${readText}`;
+const readStatusSelect = document.createElement('select');
+const readStatusOptions = ['Yes', 'No', 'In progress'];
+  readStatusSelect.innerHTML = readStatusOptions.map(option => `<option value="${option}">${option}</option>`).join('');
+  readStatusSelect.value = readText;
+  readStatusSelectDiv.appendChild(readStatusSelect);
 
-    // Append card content
-    card.appendChild(title);
-    card.appendChild(author); 
-    card.appendChild(pages);
-    card.appendChild(read);
+// Select change handler
+readStatusSelect.addEventListener('change', () => {
+  book.read = readStatusSelect.value === 'Yes'; 
+});
 
-    // Add card to list
-    booksList.appendChild(card);
+// Append card content
+card.appendChild(title);
+card.appendChild(author); 
+card.appendChild(pages);
+card.appendChild(readStatusSelectDiv);
+// Add card to list
+booksList.appendChild(card);
+
+// Remove button container
+const removeBtnContainer = document.createElement('div');
+  removeBtnContainer.classList.add('removeContainer');
+
+// Remove button
+const index = books.indexOf(book);
+const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+
+  removeBtn.addEventListener('click', () => {
+    // Remove book from array
+    books.splice(index, 1);
+    // Remove card from list
+    booksList.removeChild(card);
+  });
+  // Append button to container
+  removeBtnContainer.appendChild(removeBtn);
+  // Append container to card
+  card.appendChild(removeBtnContainer);
 
   });
 
